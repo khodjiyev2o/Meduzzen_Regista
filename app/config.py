d18e575e7f69d3b700fb1e6cd69a37ac0a9ecd20
samandar <samandar@samandarui-MacBookAir.local>
2022-11-16 22:50:53 +0900

    dockerfile added

---
+++ app/config.py
@@ -0,0 +1,27 @@
+
+
+from fastapi.middleware.cors import CORSMiddleware
+# from fastapi_pagination import add_pagination
+# from routers.users import user_router
+# from routers.companies import company_router
+# from routers.tasks import task_router
+
+
+def configure(app):
+    origins = [
+        "http://localhost",
+        "http://localhost:8000",
+    ]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    
+    # add_pagination(app)
+    # app.include_router(user_router)
+    # app.include_router(company_router)
+    # app.include_router(task_router)
